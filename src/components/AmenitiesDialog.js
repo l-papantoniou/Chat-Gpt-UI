2e370d46fdb01f43a21aa7254c5e8d4fadfb4f3c
lpapantoniou <lpapantoniou@knowledge.gr>
2024-01-28 00:22:31 +0200

    minor changes in amenities dialog selection

--- src/components/AmenitiesDialog.js
+++ src/components/AmenitiesDialog.js
@@ -5,15 +5,15 @@ import {
 } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import CloseIcon from '@mui/icons-material/Close';
-import { amenityOptions } from "../statics/amenityOptions";
+import {amenityOptions} from "../statics/amenityOptions";
 
-const AmenitiesDialog = ({ open, onClose, selectedAssets, onAmenityToggle }) => {
+const AmenitiesDialog = ({open, onClose, selectedAssets, onAmenityToggle}) => {
 
     const isAmenitySelected = (category, asset) => selectedAssets[category]?.includes(asset);
 
     return (
         <Dialog open={open} onClose={onClose} fullWidth maxWidth="md">
-            <DialogTitle sx={{ m: 0, p: 2 }}>
+            <DialogTitle sx={{m: 0, p: 2}}>
                 Select Amenities & Assets
                 <IconButton
                     aria-label="close"
@@ -25,22 +25,28 @@ const AmenitiesDialog = ({ open, onClose, selectedAssets, onAmenityToggle }) => 
                         color: (theme) => theme.palette.grey[500],
                     }}
                 >
-                    <CloseIcon />
+                    <CloseIcon/>
                 </IconButton>
             </DialogTitle>
             <DialogContent>
-                {Object.entries(amenityOptions).map(([category, { icon, amenities }]) => (
+                {Object.entries(amenityOptions).map(([category, {icon, amenities}]) => (
                     <Accordion key={category}>
-                        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                        <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
                             <ListItemIcon>{icon}</ListItemIcon>
-                            <ListItemText primary={category} />
+                            <ListItemText primary={category}/>
                         </AccordionSummary>
                         <AccordionDetails>
                             <List>
                                 {amenities.map((amenity) => (
                                     <ListItem key={amenity} button onClick={() => onAmenityToggle(category, amenity)}>
-                                        <Checkbox checked={isAmenitySelected(category, amenity)} />
-                                        <ListItemText primary={amenity} />
+                                        <Checkbox checked={isAmenitySelected(category, amenity)}
+                                                  color="secondary"
+                                                  size="medium"
+                                        />
+                                        <ListItemText primary={amenity}
+                                                      primaryTypographyProps={{
+                                                          style: {fontWeight: isAmenitySelected(category, amenity) ? 'bold' : 'normal'}
+                                                      }}/>
                                     </ListItem>
                                 ))}
                             </List>
